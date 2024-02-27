73342c7ff0980254350d38bc8074c0138498c857
Kateryna Vitruk <katherina.vitruk@gmail.com>
2024-02-27 22:12:50 +0200

    (leetcode): [8.excel-sheet-column-title.js] description

--- leetcode/8.excel-sheet-column-title.js
+++ leetcode/8.excel-sheet-column-title.js
@@ -2,6 +2,31 @@
  * Link: https://leetcode.com/problems/excel-sheet-column-title/
  *
  * Description:
+ * Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+ *
+ * For example:
+ *
+ * A -> 1
+ * B -> 2
+ * C -> 3
+ * ...
+ * Z -> 26
+ * AA -> 27
+ * AB -> 28
+ * ...
+ *
+ * Example 1:
+ *
+ * Input: columnNumber = 1
+ * Output: "A"
+ * Example 2:
+ *
+ * Input: columnNumber = 28
+ * Output: "AB"
+ * Example 3:
+ *
+ * Input: columnNumber = 701
+ * Output: "ZY"
  */
 
 // Using ASCII table where A = "26" and Z = "90"
