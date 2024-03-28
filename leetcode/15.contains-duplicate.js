1ad12339e14add26d7b15fde1c83527aa467e0a3
Kateryna Vitruk <katherina.vitruk@gmail.com>
2024-03-28 19:48:43 +0200

    (leetcode): [15.contains-duplicate] solution

---
+++ leetcode/15.contains-duplicate.js
@@ -0,0 +1,38 @@
+/**
+ * Link: https://leetcode.com/problems/contains-duplicate/
+ *
+ * Description:
+ * Given an integer array nums, return true if any value appears at least twice in the array,
+ * and return false if every element is distinct.
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ * Example 2:
+ *
+ * Input: nums = [1,2,3,4]
+ * Output: false
+ * Example 3:
+ *
+ * Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ */
+
+const containsDuplicate = function(nums) {
+    const seen = new Set();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (seen.has(nums[i])) {
+            return true;
+        }
+
+        seen.add(nums[i]);
+    }
+
+    return false;
+};
+
+containsDuplicate([1, 2, 3, 1]); // true
+containsDuplicate([1, 2, 3, 4]); // false
+containsDuplicate([1,1,1,3,3,4,3,2,4,2]); // true
